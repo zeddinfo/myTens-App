76741d90d178a1d6663504fa325cfaec50914f91
adisiswoyo <adisiswoyo202@gmail.com>
2022-03-13 11:49:51 +0700

    add state initial to handle in list card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [repo, setRepo] = useState([]);
+  const [page, setPage] = useState(1);
+  const [user, setUser] = useState('zeddinfo');
+  const [isSearch, setSearch] = useState(false);
+  const [loading, isLoading] = useState(false);
+
   return (
     <div className="App">
       
