767f8ae2c46033b2fc9a337c3b83a08285ccab0c
adisiswoyo <adisiswoyo202@gmail.com>
2022-03-13 11:50:37 +0700

    add init api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -9,6 +10,27 @@ function App() {
   const [user, setUser] = useState('zeddinfo');
   const [isSearch, setSearch] = useState(false);
   const [loading, isLoading] = useState(false);
+
+  const _initApi = async (name) => {
+    isLoading(true);
+    await axios.get(`https://api.github.com/users/${name}/repos?per_page=10&page=${page}`)
+    .then(res => {
+      const data = res.data;
+      console.log('data', data);
+      setRepo(data);
+      isLoading(false);
+    })
+    .catch((error) => {
+      console.log('error call api', error);
+      setRepo([]);
+      isLoading(false);
+    })
+  }
+
+  const handleSearch = () => {
+    _initApi(user);
+  }
+
 
   return (
     <div className="App">
