3c0eb886e66c83e645424e36e895fafa28472e1d
adisiswoyo <adisiswoyo202@gmail.com>
2022-03-13 11:51:23 +0700

    add layaout app and list card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import ListCard from './components/listCard';
 
 function App() {
 
@@ -34,7 +35,21 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div className="App">
+      <div style={{padding: 20, alignContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
+        <h2 style={{textAlign: 'center'}}>Repo App</h2>
+      </div>
+      <div style={{padding: 20, alignContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
+        <input type="text" style={{height: 30, borderRadius: 5, backgroundColor: '#f5f5f5', padding: 8}} placeholder="Cari User" value={user} onChange={(e) => setUser(e.target.value)}/>
+        <button style={{height: 30, borderRadius: 5, padding: 5, marginLeft: 10}} onClick={() => handleSearch()}>
+          Search User
+        </button>
+      </div>
+      {repo.length > 0 ? repo.map((item, index) => {
+         return <ListCard item={item}/>
+       }) : <div style={{alignContent: 'center', justifyContent: 'center', alignItems: 'center'}}><p>Data tidak ditemukan</p></div>}
+
+    </div>
     </div>
   );
 }
